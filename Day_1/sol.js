97080baad4c3f525b2f905f27f5ce6001c530161
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-12 23:24:16 +0530

    Solution : Middle of a Linked List

--- Day_1/sol.js
+++ Day_1/sol.js
@@ -1,12 +1,12 @@
 class Solution {
     /* Should return data of middle node. If linked list is empty, then  -1*/
-    int getMiddle(Node node) {
-        Node slow = node;
-        Node fast = node;
-        while(fast != null && fast.next != null){
+    getMiddle(node) {
+        let slow = head;
+        let fast = head;
+        while(fast !== null && fast.next !== null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        return  slow.data;
+        return slow.data;
     }
 }
